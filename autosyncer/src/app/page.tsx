6f36f160976c7adaf8febfa3f9b79b8f7a2c56cf
Jaya <jaya.lohana24@gmail.com>
2024-04-02 22:44:28 -0400

    first commit

---
+++ autosyncer/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/global/navbar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar />
+      <section className="h-screen w-full  bg-neutral-950 rounded-md  !overflow-visible relative flex flex-col items-center  antialiased">
+        <div className="absolute inset-0  h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>
+      </section>
+    </main>
+  );
+}
