6d1b18d43ff8c8e18e6f3510ebf32eeb513b095d
Jaya <jaya.lohana24@gmail.com>
2024-04-03 22:43:21 -0400

    changes

--- autosyncer/src/app/page.tsx
+++ autosyncer/src/app/page.tsx
@@ -1,3 +1,4 @@
+import { ContainerScroll } from "@/components/global/container-scroll-animation";
 import Navbar from "@/components/global/navbar";
 import Image from "next/image";
 
@@ -7,6 +8,13 @@ export default function Home() {
       <Navbar />
       <section className="h-screen w-full  bg-neutral-950 rounded-md  !overflow-visible relative flex flex-col items-center  antialiased">
         <div className="absolute inset-0  h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>
+        <div className="flex flex-col mt-[-100px] md:mt-[-50px]">
+          <ContainerScroll
+            titleComponent={
+              <div className="flex items-center justify-center flex-col"></div>
+            }
+          ></ContainerScroll>
+        </div>
       </section>
     </main>
   );
