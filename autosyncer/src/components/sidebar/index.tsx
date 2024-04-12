911d04ac1f460796bd219720f0b95406385f6ce2
Jaya <jaya.lohana24@gmail.com>
2024-04-12 11:06:20 -0400

    changes

--- autosyncer/src/components/sidebar/index.tsx
+++ autosyncer/src/components/sidebar/index.tsx
@@ -10,6 +10,9 @@ import {
 } from "@/components/ui/tooltip";
 import { menuOptions } from "@/lib/constant";
 import clsx from "clsx";
+import { Separator } from "../ui/separator";
+import { Database, GitBranch, LucideMousePointerClick } from "lucide-react";
+import { ModeToggle } from "../global/mode-toggle";
 
 type Props = {};
 
@@ -32,7 +35,7 @@ const MenuOptions = (props: Props) => {
                     <Link
                       href={menuItem.href}
                       className={clsx(
-                        "group h-8 w-8 flex items-center justify-center  scale-[1.5] rounded-lg p-[3px]  cursor-pointer",
+                        "group h-8 w-8 flex items-center justify-center scale-[1.5] rounded-lg p-[3px]  cursor-pointer",
                         {
                           "dark:bg-[#2F006B] bg-[#EEE0FF] ":
                             pathName === menuItem.href,
@@ -55,6 +58,27 @@ const MenuOptions = (props: Props) => {
             </ul>
           ))}
         </TooltipProvider>
+        <Separator />
+        <div className="flex items-center flex-col gap-9 dark:bg-[#353346]/30 py-4 px-2 rounded-full h-56 overflow-scroll border-[1px]">
+          <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
+            <LucideMousePointerClick className="dark:text-white" size={18} />
+            <div className="border-l-2 border-muted-foreground/50 h-6 absolute left-1/2 transform translate-x-[-50%] -bottom-[30px]" />
+          </div>
+          <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
+            <GitBranch className="text-muted-foreground" size={18} />
+            <div className="border-l-2 border-muted-foreground/50 h-6 absolute left-1/2 transform translate-x-[-50%] -bottom-[30px]"></div>
+          </div>
+          <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
+            <Database className="text-muted-foreground" size={18} />
+            <div className="border-l-2 border-muted-foreground/50 h-6 absolute left-1/2 transform translate-x-[-50%] -bottom-[30px]"></div>
+          </div>
+          <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
+            <GitBranch className="text-muted-foreground" size={18} />
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center justify-center flex-col gap-8">
+        <ModeToggle />
       </div>
     </nav>
   );
