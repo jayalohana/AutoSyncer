87e654a1aaa653d9a6a6f271c42e85e0f2269b99
Jaya <jaya.lohana24@gmail.com>
2024-04-14 11:11:50 -0400

    changes

--- autosyncer/src/components/forms/profile-form.tsx
+++ autosyncer/src/components/forms/profile-form.tsx
@@ -5,6 +5,17 @@ import React, { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { EditUserProfileSchema } from "@/lib/types";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { Loader2 } from "lucide-react";
 
 type Props = {};
 
@@ -19,7 +30,63 @@ const ProfileForm = (props: Props) => {
     },
   });
 
-  return <div>ProfileForm</div>;
+  const handleSubmit = async (data: any) => {
+    setIsLoading(true);
+    // Perform form submission logic here
+    setIsLoading(false);
+  };
+
+  return (
+    <Form {...form} onSubmit={form.handleSubmit(handleSubmit)}>
+      <div className="flex flex-col gap-6">
+        <FormField
+          disabled={isLoading}
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-lg">Full name</FormLabel>
+              <FormControl>
+                <Input {...field} placeholder="Name" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-lg">Email</FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  disabled={true}
+                  placeholder="Email"
+                  type="email"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button
+          type="submit"
+          className="self-start hover:bg-[#2F006B] hover:text-white "
+        >
+          {isLoading ? (
+            <>
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              Saving
+            </>
+          ) : (
+            "Save User Settings"
+          )}
+        </Button>
+      </div>
+    </Form>
+  );
 };
 
 export default ProfileForm;
